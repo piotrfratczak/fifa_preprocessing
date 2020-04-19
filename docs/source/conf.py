6c11ba018e4c68c4491b570916b927da805e873a
Piotr Frątczak <piotrfratczak99@gmail.com>
2020-04-19 16:29:10 +0200

    Fix autodoc build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../fifa_preprocessing'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
